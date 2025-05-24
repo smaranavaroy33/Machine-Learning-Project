63f51ef7061d639c06e4ccf3ed5ca7afeca781c0
smaranavaroy33 <roy.smaranavaroy.com>
2025-05-24 11:43:57 +0530

    exception and logging

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+import sys
+
+def error_message_detail(error,error_detail:sys):
+    """
+    This function takes an error and its details as input and returns a formatted string with the error message.
+    """
+    _,_,exc_tb = error_detail.exe_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occured in script name [{file_name}] at line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+    return error_message
+
+class CustomException(Exception):
+    """
+    This class is a custom exception that inherits from the built-in Exception class.
+    It takes an error and its details as input and returns a formatted string with the error message.
+    """
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+    
